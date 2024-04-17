47ff4f5371c32a7876e550715c5012065f5fce1a
Jow Xavier <jonathanxribeiro@gmail.com>
2024-04-17 11:15:06 -0300

    feat: add Product Domain

---
+++ tests/Domain/Product/Entities/ProductTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use DomainException;
+use App\DomainDrivenDesign\Domain\Product\Entities\Product;
+
+$product = new Product(1, 'item', 10);
+it('should returns object', fn() => expect($product)->toBeObject());
+
+it('should throw name validation exception', function () {
+    new Product(1, '', 10);
+})->throws(DomainException::class, 'Name is not valid.');
+
+it('should throw price validation exception', function () {
+    new Product(1, 'item', 0);
+})->throws(DomainException::class, 'Price is not valid.');
