d89a54331939334e9e8005e9c90fee7b059425ee
Jow Xavier <jonathanxribeiro@gmail.com>
2024-04-18 17:02:54 -0300

    feat: Introduce customer repositories

---
+++ src/Domain/Customer/Repositories/CustomerRepositoryInterface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\DomainDrivenDesign\Domain\Customer\Repositories;
+
+use App\DomainDrivenDesign\Domain\Repositories\RepositoryInterface;
+
+interface CustomerRepositoryInterface extends RepositoryInterface
+{}
